551ba2e45294c6c0b1113dafb5ba22af58da1745
Arlind Ukshini <quka93@gmail.com>
2018-11-23 13:07:20 +0100

    d inc

--- motionsberegner.py
+++ motionsberegner.py
@@ -5,7 +5,7 @@ import json
 
 sense = SenseHat() # init sensehat
 timestamp = datetime.now() # set timer for use later
-delay = 0.1 # delay in seconds (1/10 of a sec)
+delay = 0.2 # delay in seconds (1/10 of a sec)
 
 def setup_udp_socket():
     # Setup UDP socket for broadcasting
