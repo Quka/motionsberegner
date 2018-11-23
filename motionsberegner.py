e04f714ed2f6e86547681b57bc4976a10e7ced75
Arlind Ukshini <quka93@gmail.com>
2018-11-23 10:43:42 +0100

    init udp broadcaster

--- motionsberegner.py
+++ motionsberegner.py
@@ -3,3 +3,46 @@ import socket
 from datetime import datetime
 import json
 
+sense = SenseHat() # init sensehat
+timestamp = datetime.now() # set timer for use later
+delay = 0.1 # delay in seconds (1/10 of a sec)
+
+def setup_udp_socket():
+    # Setup UDP socket for broadcasting
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    # Set a timeout so the socket does not block
+    # indefinitely when trying to receive data.
+    server.settimeout(0.2)
+
+    server.bind(("", 44444))
+    return server
+
+def get_sense_data():
+    sense_data = {
+        'temp': round(sense.get_temperature(), 1),
+        'press': round(sense.get_pressure(), 1),
+        'hum': round(sense.get_humidity(), 1),
+        'date': datetime.now()
+    }
+    
+    return sense_data
+
+# init server
+server = setup_udp_socket()
+
+while True:
+    data = get_sense_data()
+    time = data["date"] - timestamp # træk timestamp fra datetime i data
+
+    # Sæt et delay for hvor ofte den skal læse data (delay = 1 sekund)
+    if time.seconds > delay:
+        # Convert dictionary to JSON Object (str) and then to bytes
+        dataBytes = (json.dumps(data, default=str)).encode()
+
+        # Broadcast message to port 37020 via UDP Socket
+        server.sendto(dataBytes, ('<broadcast>', 37020))
+
+        # Show a message on the display
+        sense.show_message( "sent", scroll_speed=0.05 )
