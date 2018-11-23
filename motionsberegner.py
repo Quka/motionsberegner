1cc847e450c0cc36f2764ecda7b42f61c5efd132
Arlind Ukshini <quka93@gmail.com>
2018-11-23 10:38:32 +0100

    fix import

--- motionsberegner.py
+++ motionsberegner.py
@@ -1,20 +1,5 @@
-import importlib
+from sense_hat import SenseHat
 import socket
 from datetime import datetime
 import json
 
-def find_module(full_module_name):
-    """
-    Returns module object if module `full_module_name` can be imported. 
-
-    Returns None if module does not exist. 
-
-    Exception is raised if (existing) module raises exception during its import.
-    """
-    try:
-        return importlib.import_module(full_module_name)
-    except ImportError as exc:
-        if not (full_module_name + '.').startswith(exc.name + '.'):
-            raise
-
-find_module("SenseHat")
