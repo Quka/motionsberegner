98a668ab45e41efacc2fe1cf919811d1ac0a64ce
Søren Lykke Christensen <soer424c@edu.easj.dk>
2018-11-27 11:54:35 +0100

    s

--- tracker/Testing_zone.py
+++ tracker/Testing_zone.py
@@ -42,9 +42,9 @@ def get_sense_data():
 server = setup_udp_socket()
 
 
-with open("C:\source\Important tasks\3.sem Project\Accl_data.csv", "a") as log
+with open("C:\source\Important tasks\3.sem Project\Accl_data.csv", "a") as log:
 
 # send the data with udp
-while True:
-    data = get_sense_data
-    log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(data)))
+    while True:
+        data = get_sense_data
+        log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(data)))
