976fcb212626c134a2ca522c4f5970451f4cf123
Søren Lykke Christensen <soer424c@edu.easj.dk>
2018-11-27 11:12:08 +0100

    lol

--- tracker/motionsberegner.py
+++ tracker/motionsberegner.py
@@ -30,8 +30,8 @@ def get_sense_data():
     sense_data = {
         'x': abs(round(accl['x'], 2)),
         'y': abs(round(accl['y'], 2)),
-        'z': abs(round(accl['z'], 2))
-        #'date': datetime.now()
+        'z': abs(round(accl['z'], 2)),
+        'date': datetime.now()
     }
 
     return sense_data
@@ -42,13 +42,13 @@ server = setup_udp_socket()
 # send the data with udp
 while True:
     data = get_sense_data()
-    #time = data["date"] - timestamp # træk timestamp fra datetime i data
+    time = data["date"] - timestamp # træk timestamp fra datetime i data
 
     # Sæt et delay for hvor ofte den skal læse data (delay = 1 sekund)
-   # if time.seconds > delay:
+    if time.seconds > delay:
 
-        # For testing the values of a step
-        print(data)
+        
+        print(data["x"], data["y"], data["z"])
 
         # Convert dictionary to JSON Object (str) and then to bytes
         dataBytes = (json.dumps(data, default=str)).encode()
