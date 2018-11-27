def6081344645ed073b295c50031b99cb2492c9c
Søren Lykke Christensen <soer424c@edu.easj.dk>
2018-11-27 13:02:45 +0100

    EEER

--- tracker/Testing_zone.py
+++ tracker/Testing_zone.py
@@ -11,7 +11,7 @@ sense = SenseHat() # init sensehat
 timestamp = datetime.now() # set timer for use later
 delay = 0.2 # delay in seconds (1/10 of a sec)
 
-
+steps = 0
 
 def get_sense_data():
     acc = sense.get_accelerometer_raw()
@@ -21,18 +21,15 @@ def get_sense_data():
     sense_data.append(acc["z"])
     
     return sense_data
-steps = 0
-def step_counter():
-    
-    
 
+def step_counter():
     x, y, z = sense.get_accelerometer_raw().values()
 
     if x>1.1:
         steps = steps + 1
     if y>1.1:
         steps = steps + 1
-    return steps
+return steps
 
 
 while True:
