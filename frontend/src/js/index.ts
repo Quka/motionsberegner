b9a9c88ec9462df914539fc2199b5859801dfc7b
Hakan Aslan <haka0089@edu.easj.dk>
2018-11-28 13:03:48 +0100

    patch 0.2

--- frontend/src/js/index.ts
+++ frontend/src/js/index.ts
@@ -1,1 +1,7 @@
+import axios, { AxiosResponse, AxiosError} from "../../node_modules/axios/index";
 
+import { Profile } from "./Profile";
+
+let p = new Profile("Hakan");
+
+console.log(p.showName());
