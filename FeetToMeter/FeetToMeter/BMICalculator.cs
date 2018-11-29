33bda238973b651536a66a894c979613583d8deb
God4you <37441035+God4you@users.noreply.github.com>
2018-11-29 11:12:40 +0100

    Add files via upload

---
+++ FeetToMeter/FeetToMeter/BMICalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MotionsberegnerApp
+{
+   public class BMICalculator
+    {
+        public int CalcBMI(int kg, int height)
+        {
+            double bmi;
+            bmi = kg / Math.Pow(height / 100.0, 2);
+
+            bmi = Math.Round(bmi, 0);
+
+            return Int32.Parse(bmi.ToString());
+        }
+
+    }
+}
---
+++ FeetToMeter/FeetToMeter/StepCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MotionsberegnerApp
+{
+    public class StepCalculator
+    {
+        double meter, km; //double variabler
+        public double StepsToMeter(int steps)
+        {
+            
+            //Console.BackgroundColor = ConsoleColor.Black; //Baggrunds farve
+            //Console.ForegroundColor = ConsoleColor.White; // Forgrunds farve
+            //Console.Write("\nEnter steps: ");
+            // steps = Convert.ToInt32(Console.ReadLine()); //Input bliver = steps.
+            //Console.ResetColor(); // Tilbage til normal farve
+            meter = steps * 0.79; //Steps bliver ganget med 0.79. 
+            meter = Math.Round(meter, 2); // Vil kun have de 2 decimal med.     
+            //Console.BackgroundColor = ConsoleColor.DarkCyan;
+            //Console.ForegroundColor = ConsoleColor.White;
+            //Console.WriteLine("\nSteps in meter: " + meter); // Printer meter ud. 
+            km = meter / 1000; // regner meter om til km.
+            km = Math.Round(km, 3); //For at vise 3 decimaler. 
+            //Console.WriteLine("\nMeter in Kilometer: " + km);
+            //Console.ResetColor();
+
+            return meter;
+        }
+
+        public double StepsToKilometer(int steps)
+        {
+            meter = steps * 0.79;
+            meter = Math.Round(meter, 2);
+            km = meter / 1000;
+            km = Math.Round(km, 2);
+
+            return km;
+        }
+    }
+}
