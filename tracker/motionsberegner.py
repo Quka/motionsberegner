6f9799e0dbbd8e16283dc1271288e9cf138f401a
Arlind Ukshini <quka93@gmail.com>
2018-11-29 14:07:21 +0100

    send data

--- tracker/motionsberegner.py
+++ tracker/motionsberegner.py
@@ -69,6 +69,7 @@ def get_sense_data_array():
 server = setup_udp_socket()
 datalog = []
 
+'''
 # Save data with file
 with open('./data2.csv', 'w', newline='') as csvfile:
     data_writer = writer(csvfile)
@@ -82,9 +83,9 @@ with open('./data2.csv', 'w', newline='') as csvfile:
         data_writer.writerow(['x', 'y', 'z', 'date'])
         print(data)
             #timestamp = datetime.now()
-    
+'''  
 
-'''
+
 # Save data with array
 while True:
 
@@ -107,4 +108,3 @@ while True:
 
     # Sæt et delay for hvor ofte den skal læse data (delay = 1 sekund)
     #if time > delay:
-'''
