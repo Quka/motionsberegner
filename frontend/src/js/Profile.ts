589471a5bcd3bc0aac895b583152e091c8bf8af8
Hakan Aslan <haka0089@edu.easj.dk>
2018-12-03 09:37:51 +0100

    patch 0.4.1

--- frontend/src/js/Profile.ts
+++ frontend/src/js/Profile.ts
@@ -18,5 +18,41 @@ export class Profile {
 }
 
 interface IProfile {
-    
+ firstName : string;
+ lastName : string;
+ age : number;
+ 
+}
+
+let resultOfProfile : HTMLDivElement = <HTMLDivElement> document.getElementById("userResult")
+
+function getProfile(): void {
+let uri : string = "http://motionsberegner.database.windows.net/profile" 
+let result  = uri 
+axios.get<IProfile>(result)
+   .then(function(response){
+    resultOfProfile.innerHTML =  response.data.firstName + "   " + response.data.lastName + "   "  + response.data.age;
+  })
+
+}
+
+
+function createNewCustomer(): void {
+let uri : string = "http://motionsberegner.database.windows.net";
+
+let id : HTMLInputElement = document.getElementById("id") as HTMLInputElement;
+let firstName : HTMLInputElement  = document.getElementById("firstName") as HTMLInputElement;
+let lastName : HTMLInputElement = document.getElementById("lastName") as HTMLInputElement;
+let age : HTMLInputElement = document.getElementById("year") as HTMLInputElement;
+let result : ICustomer = {id : id.valueAsNumber , firstName: firstName.value , lastName : lastName.value, year : year.valueAsNumber};
+
+axios.post<ICustomer>(uri, result)
+}
+
+function deleteCustomer(): void {
+let uri : string ="http://motionsberegner.database.windows.net"
+let id : HTMLInputElement = document.getElementById("deleteId") as HTMLInputElement;
+
+let result = uri + id.value;
+axios.delete(result);
 }
--- frontend/src/js/index.ts
+++ frontend/src/js/index.ts
@@ -5,17 +5,28 @@ import { Profile } from "./Profile";
 let p = new Profile("Rasmus");
 console.log(p.showName());
 
-
+let lastPage = "";
 let element: HTMLDivElement = <HTMLDivElement>document.getElementById("content");
 let btn1: HTMLButtonElement = <HTMLButtonElement>document.getElementById("loginButton");
 let btn2: HTMLButtonElement = <HTMLButtonElement>document.getElementById("opretButton");
-
-btn1.addEventListener('click', removeToLogin);
+let backButton: HTMLButtonElement = <HTMLButtonElement> document.getElementById("backButton")
+btn1.addEventListener('click', removeToProfil);
 btn2.addEventListener('click', removeToOpret);
+backButton.addEventListener('click', backToHomePage);
 
 
+function homepage(): void 
+{
+    lastPage = "homepage";
+    //HOME PAGE
+    "<input id=input placeholder='Indtast brugernavn her'><br>"
+    "<input id=input placeholder='Indtast kodeord her'>"
+    "<button id=loginButton>Login</button>"
+    "<button id=opretButton>Opret profil</button>"
+}
 
 function page1(): void {
+    lastPage = "page1";
     let html = "";
     html = "<h2>Profil</h2> ";
     html += "<h6>Brugeroplysninger</h6><br>";
@@ -30,13 +41,37 @@ function page1(): void {
     // Indsæt database data her
     "<h7>Højde</h7><br><br>" + 
     // Indsæt database data her
-    "<h7>Antal skridt</h7><br><br>";
+    "<h7>Antal skridt</h7><br><br>" +
 
-    
+
+    "<button id='backButton'>Tilbage</button>"+
+
+
+    "<div id='userResult'></div>";
+    interface IProfile {
+        firstName : string;
+        lastName : string;
+        age : number;
+        
+       }
+    let resultOfProfile : HTMLDivElement = <HTMLDivElement> document.getElementById("userResult")
+    function getProfile(): void {
+        let uri : string = "http://motionsberegner.database.windows.net/profile" 
+        let result  = uri 
+        axios.get<IProfile>(result)
+           .then(function(response){
+            resultOfProfile.innerHTML =  response.data.firstName + "   " + response.data.lastName + "   "  + response.data.age;
+          })
+        
+        }
+    getProfile();
+
+
     element.innerHTML = html;  
 }
 
 function page2(): void {
+    lastPage = "page2";
     let html = "";
     html = "<h2>Opret profil</h2> ";
     html += "<h6>Brugeroplysninger</h6><br>";
@@ -52,14 +87,12 @@ function page2(): void {
     "<h7>Højde</h7><br>" + 
     "<input id=input placeholder='Indtast højde her'><br><br>";
     
-    
     element.innerHTML = html;
-
-    
 }
 
 
-function removeToLogin() : void {
+
+function removeToProfil() : void {
     // Removes an element from the document
     var element = document.getElementById("content");
     element.innerHTML = "";
@@ -71,4 +104,18 @@ function removeToOpret() : void {
     var element = document.getElementById("content");
     element.innerHTML = "";
     page2();
+}
+
+function backToHomePage(): void {
+    // Removes an element from the document
+    //var element = document.getElementById("content");
+    if(lastPage = "page1") {
+        page1();
+    }
+    else if(lastPage = "page2") {
+        page2();
+    }
+    else if(lastPage = "homepage") {
+        homepage();
+    }
 }
